d6a02ff28e86e5e8570be9f96eb65b06ef42ec40
brandan.neises <brandan.neises@phantom.us>
2018-06-29 09:43:11 -0500

    PAPP-4700 Add hostname to configuration to allow for on-prem version of Anomali.

--- threatstream_connector.py
+++ threatstream_connector.py
@@ -1,16 +1,8 @@
-# --
 # File: threatstream_connector.py
+# Copyright (c) 2016-2018 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2016-2018
-#
-# This unpublished material is proprietary to Phantom Cyber Corporation.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber Corporation.
-#
-# --
+# SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 
 # Phantom imports
 import phantom.app as phantom
